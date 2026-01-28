ccd6ae7c93ec055cb2c58b6c4c3abdc217691bc8
Brayanklein555 <brayanprogramacao@gmail.com>
2026-01-27 21:34:38 -0300

    Add UI object with show method to manage active pages

---
+++ js/ui.js
@@ -0,0 +1,6 @@
+const UI = {
+    show(id){
+        document.querySelectorAll(".page").forEach(p=>p.classList.remove("active"));
+        document.getElementById(id).classList.add("active");
+    }
+};
