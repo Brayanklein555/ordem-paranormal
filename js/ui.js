23f9e7331a08cc9c68e265cabaf52c1959d77f01
Brayanklein555 <brayanprogramacao@gmail.com>
2026-01-28 00:00:41 -0300

    Play click sound when showing a page
    Add audio feedback when showing a page.

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,11 @@
 const UI = {
     show(id){
-        document.querySelectorAll(".page").forEach(p=>p.classList.remove("active"));
+        AudioSystem.playClick();
+
+        document.querySelectorAll(".page").forEach(p=>{
+            p.classList.remove("active");
+        });
+
         document.getElementById(id).classList.add("active");
     }
 };
