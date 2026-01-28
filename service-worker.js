67207b30ca08163570d7b06e1cac8a179c9593b9
Brayanklein555 <brayanprogramacao@gmail.com>
2026-01-28 16:28:59 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,27 @@
-self.addEventListener("install", e=>{
-    e.waitUntil(
-        caches.open("ordo").then(c=>c.addAll(["./","./index.html","./css/style.css"]))
-    );
+const CACHE = "ordo-vtt-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./css/style.css",
+  "./js/ui.js",
+  "./js/audio.js",
+  "./js/dice.js",
+  "./js/map.js",
+  "./js/character.js",
+  "./js/combat.js",
+  "./js/pwa.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
 });
 
-self.addEventListener("fetch", e=>{
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
