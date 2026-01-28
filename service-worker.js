5d7ef7d6874a035fef8c612046f88bdf90d35b20
Brayanklein555 <brayanprogramacao@gmail.com>
2026-01-27 23:33:25 -0300

    Implement service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", e=>{
+    e.waitUntil(
+        caches.open("ordo-cache").then(cache=>{
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./css/style.css",
+                "./js/main.js"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", e=>{
+    e.respondWith(
+        caches.match(e.request).then(resp=>{
+            return resp || fetch(e.request);
+        })
+    );
+});
